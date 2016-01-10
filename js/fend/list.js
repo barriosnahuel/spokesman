962a1d3236c5c47f188a6f218a9adc2f28c49908
Nahuel Barrios <barrios.nahuel@gmail.com>
2016-01-10 14:11:11 -0300

    Browser action now opens the settings page in a new tab.

--- js/fend/list.js
+++ js/fend/list.js
@@ -4,35 +4,39 @@
 
 var spk = spk || {};
 
-$(document).ready(function () {
-    console.log('==>document.ready');
+chrome.tabs.create({
+    url: 'views/settings.html'
+}, undefined);
 
-    spk.lib.helloWorld();
-    spk.templates.register();
-
-    var quantity = $('.spk-quantity');
-    var list = $('#notifications');
-
-    spk.lib.getFeed(function (err, notifications) {
-        if (err) {
-            alert('error!!');
-        } else {
-            quantity.text(notifications.length);
-
-            for (var i = 0; i < notifications.length; i++) {
-                var html;
-                if (i % 2 == 0) {
-                    html = $.render.comment({
-                        user: 'Nahuel Barrios'
-                    });
-                } else {
-                    html = $.render.branchUpdate({
-                        user: 'Nahuel Barrios'
-                    });
-                }
-
-                list.append('<li>' + html + '</li>');
-            }
-        }
-    });
-});
+//$(document).ready(function () {
+//    console.log('==>document.ready');
+//
+//    spk.lib.helloWorld();
+//    spk.templates.register();
+//
+//    var quantity = $('.spk-quantity');
+//    var list = $('#notifications');
+//
+//    spk.lib.getFeed(function (err, notifications) {
+//        if (err) {
+//            alert('error!!');
+//        } else {
+//            quantity.text(notifications.length);
+//
+//            for (var i = 0; i < notifications.length; i++) {
+//                var html;
+//                if (i % 2 == 0) {
+//                    html = $.render.comment({
+//                        user: 'Nahuel Barrios'
+//                    });
+//                } else {
+//                    html = $.render.branchUpdate({
+//                        user: 'Nahuel Barrios'
+//                    });
+//                }
+//
+//                list.append('<li>' + html + '</li>');
+//            }
+//        }
+//    });
+//});
