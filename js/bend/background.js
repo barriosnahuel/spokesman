45f108b7d6f6e444fdc446766fbe77b0e4600478
Nahuel Barrios <barrios.nahuel@gmail.com>
2016-01-12 23:44:01 -0300

    Refactor of background.js

--- js/bend/background.js
+++ js/bend/background.js
@@ -3,55 +3,20 @@
  */
 var spk = spk || {};
 
+(function () {
 
-chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('Running onInstalled, reason: %s', details.reason);
-    console.log('Previous version: %s', details.previousVersion);
+    var onExtensionInstalledOrUpdated = function (details) {
+        console.log('Running onInstalled, reason: %s', details.reason);
+        console.log('Previous version: %s', details.previousVersion);
 
-    if (details.reason === 'install') {
-        chrome.tabs.create({
-            url: 'views/settings.html'
-        }, undefined);
-    }
-});
-
-$.ajax({
-    url: 'properties.json',
-    dataType: 'json',
-    async: false
-}).done(function (properties) {
-    console.log('Properties file loaded OK');
-    spk.properties = properties;
-
-    chrome.notifications.onClicked.addListener(function (notificationId) {
-        chrome.storage.local.get(notificationId, function (data) {
-
-            if (data[notificationId]) {
-                chrome.tabs.create({
-                    url: data[notificationId]
-                }, function () {
-                    chrome.notifications.clear(notificationId);
-                });
-            }
-        });
-    });
-
-    chrome.notifications.onClosed.addListener(function (notificationId, byUser) {
-        if (byUser) {
-            console.log('notification %s %s', notificationId, ' closed by the user');
-
-            chrome.storage.local.remove(notificationId, undefined);
+        if (details.reason === 'install') {
+            chrome.tabs.create({
+                url: 'views/settings.html'
+            }, undefined);
         }
-    });
-
-    var syncLastEventRead = function (event) {
-        console.log('Last event read %s', event.id);
-
-        chrome.storage.sync.set({'lastEventId': event.id}, undefined);
     };
 
     /**
-     *
      * @param eachEvent
      * @param next
      */
@@ -63,7 +28,58 @@ $.ajax({
         });
     };
 
-    chrome.alarms.onAlarm.addListener(function (alarm) {
+    var processEvent = function (eachEvent) {
+        var dto = spk.events.manager.parse(eachEvent);
+        if (dto && spk.events.manager.shouldProcess(dto)) {
+            var notification = spk.events.manager.buildNotification(dto);
+
+            var notificationOptions = {
+                type: 'basic',
+                title: notification.title,
+                message: notification.message,
+                contextMessage: notification.contextMessage,
+                iconUrl: notification.icon || 'img/megaphone17-256.png'
+            };
+
+            chrome.notifications.create(dto.id, notificationOptions, function (notificationId) {
+                var options = {};
+                options[notificationId] = notification.link;
+                chrome.storage.local.set(options);
+            });
+        } else {
+            console.log('WARN: Event id %s (%s) was NOT handled', eachEvent.id, eachEvent.type);
+        }
+    };
+
+    var onNotificationClicked = function (notificationId) {
+        chrome.storage.local.get(notificationId, function (data) {
+
+            if (data[notificationId]) {
+                chrome.tabs.create({
+                    url: data[notificationId]
+                }, function () {
+                    chrome.notifications.clear(notificationId);
+                });
+            }
+        });
+    };
+
+    var onNotificationClosed = function (notificationId, byUser) {
+        if (byUser) {
+            console.log('notification %s %s', notificationId, ' closed by the user');
+
+            chrome.storage.local.remove(notificationId, undefined);
+        }
+    };
+
+    var onAlarmFired = function (alarm) {
+
+        var syncLastEventRead = function (event) {
+            console.log('Last event read %s', event.id);
+
+            chrome.storage.sync.set({'lastEventId': event.id}, undefined);
+        };
+
         console.log('Checking for new notifications...');
 
         spk.lib.getEvents(function (err, events) {
@@ -71,6 +87,7 @@ $.ajax({
             if (err) {
                 console.error('Can\'t get GitHub\'s events: %s', err);
             } else {
+
                 chrome.storage.sync.get(undefined, function (storage) {
                     if (storage.branches) {
                         spk.properties.push_branches = storage.branches;
@@ -81,48 +98,50 @@ $.ajax({
                     }
 
                     for (var i = events.length - 1; i >= 0; i--) {
-                        var eachEvent = events[i];
-
-                        isNotRead(eachEvent, function (eachEvent) {
-                            var dto = spk.events.manager.parse(eachEvent);
-                            if (dto && spk.events.manager.shouldProcess(dto)) {
-                                var notification = spk.events.manager.buildNotification(dto);
-
-                                var notificationOptions = {
-                                    type: 'basic',
-                                    title: notification.title,
-                                    message: notification.message,
-                                    contextMessage: notification.contextMessage,
-                                    iconUrl: notification.icon || 'img/megaphone17-256.png'
-                                };
-
-                                chrome.notifications.create(dto.id, notificationOptions, function (notificationId) {
-                                    var options = {};
-                                    options[notificationId] = notification.link;
-                                    chrome.storage.local.set(options);
-                                });
-                            } else {
-                                console.log('WARN: Event id %s (%s) was NOT handled', eachEvent.id, eachEvent.type);
-                            }
-                        });
+                        isNotRead(events[i], processEvent);
                     }
 
                     syncLastEventRead(events[0]);
                 });
+
             }
         });
+    };
+
+    // END-Declaring methods //////////////////////////////////////////////////////////////////
+
+    // ----------------------------------------------------------------------------------------
+
+    // START-Event page flow //////////////////////////////////////////////////////////////////
+
+    chrome.runtime.onInstalled.addListener(onExtensionInstalledOrUpdated);
+
+    $.ajax({
+        url: 'properties.json',
+        dataType: 'json',
+        async: false
+    }).done(function (properties) {
+        console.log('Properties file loaded OK');
+        spk.properties = properties;
+
+        chrome.notifications.onClicked.addListener(onNotificationClicked);
+
+        chrome.notifications.onClosed.addListener(onNotificationClosed);
+
+        chrome.alarms.onAlarm.addListener(onAlarmFired);
+
+        if (spk.properties.testing) {
+            chrome.storage.sync.clear();
+            chrome.storage.sync.set({'username': 'barriosnahuel'}, undefined);
+        }
+
+        chrome.alarms.create('', {
+            when: Date.now(),
+            periodInMinutes: spk.properties.testing ? undefined : 1
+        });
+
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        console.error(textStatus);
     });
 
-    if (spk.properties.testing) {
-        chrome.storage.sync.clear();
-        chrome.storage.sync.set({'username': 'barriosnahuel'}, undefined);
-    }
-
-    chrome.alarms.create('', {
-        when: Date.now(),
-        periodInMinutes: spk.properties.testing ? undefined : 1
-    });
-
-}).fail(function (jqXHR, textStatus, errorThrown) {
-    console.error(errorMessage);
-});
+}());
