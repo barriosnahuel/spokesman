2f616a4d1fe6deb5da17bd555b0ec699d2d11e33
Nahuel Barrios <barrios.nahuel@gmail.com>
2016-02-08 17:50:46 -0300

    Merge issue comment and close/reopened events into one notification.

--- js/bend/events/issue_comment_event.js
+++ js/bend/events/issue_comment_event.js
@@ -8,7 +8,8 @@ spk.events.issueCommentEvent = (function () {
 
     var parseGitHubEvent = function (event) {
         return {
-            text: event.payload.comment.body
+            action: event.payload.action
+            , text: event.payload.comment.body
             , link: event.payload.comment.html_url
             , issue: {
                 number: event.payload.issue.number
@@ -18,16 +19,48 @@ spk.events.issueCommentEvent = (function () {
     };
 
     var buildNotification = function (dto) {
-        return {
-            title: dto.actor.username + ' said on ' + dto.payload.issue.title
-            , message: dto.payload.text
-            , contextMessage: spk.util.buildNotificationContext([
-                dto.repo
-                , '#' + dto.payload.issue.number
-            ])
-            , link: dto.payload.link
-            , icon: 'img/events/comments.png'
-        };
+        var result;
+
+        var mergedActions = dto.payload.action.split('+');
+        if (mergedActions.length > 1) {
+
+            // i.e.: Issue closed w/comment by @barriosnahuel
+
+            var icon;
+            if (mergedActions[1] === 'closed') {
+                icon = 'img/events/issue-closed+commented.png';
+            } else if (mergedActions[1] === 'reopened') {
+                icon = 'img/events/issue-opened+commented.png';
+            } else {
+                console.error('Error merging actions. Can\'t create icon for IssueCommentEvent w/ action %s', mergedActions[1]);
+            }
+
+            result = {
+                title: 'Issue ' + mergedActions[1] + ' w/comment by ' + dto.actor.username
+                , message: dto.payload.issue.title + ': "' + dto.payload.text + '"'
+                , contextMessage: spk.util.buildNotificationContext([
+                    dto.repo
+                    , '#' + dto.payload.issue.number
+                ])
+                , icon: icon
+            };
+
+        } else {
+
+            result = {
+                title: dto.actor.username + ' said on ' + dto.payload.issue.title
+                , message: '"' + dto.payload.text + '"'
+                , contextMessage: spk.util.buildNotificationContext([
+                    dto.repo
+                    , '#' + dto.payload.issue.number
+                ])
+                , icon: 'img/events/comments.png'
+            };
+        }
+
+        result.link = dto.payload.link;
+
+        return result;
     };
 
     return {
